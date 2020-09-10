a6bb14a25648259564483aea149320abb0ac5d50
Alan Rocha <alan.rocha@udem.edu>
2020-09-10 13:32:39 -0500

    Fixed name

--- tests/test_dummie.py
+++ tests/test_dummie.py
@@ -1,19 +1,5 @@
-from ColorCone import colorchange as cc
-
-rgb_example = (239, 126, 115)
-rgb_float_example = (0.9372549019607843,
-                     0.49411764705882355, 0.45098039215686275)
+import pytest
 
 
-def test_modify_color():
-    result = cc.modify_rgb(rgb_example, 1.2)
-    assert isinstance(result, tuple)
-    assert result == (246, 120, 107)
-
-
-def test_rgb_float_to_int():
-    assert cc.rgb_float_to_int(rgb_float_example) == rgb_example
-
-
-def test_rgb_int_to_float():
-    assert cc.rgb_int_to_float(rgb_example) == rgb_float_example
+def test_example():
+    assert 4 == 4
--- tests/test_example.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_example():
-    assert 4 == 4
