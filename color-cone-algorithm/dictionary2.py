f9fcda462a15364acb3417e9a29fc9fea569a947
Aragon-Diego <diego.aragonCursos@gmail.com>
2020-10-21 16:20:44 -0500

    nuevo Dic

---
+++ color-cone-algorithm/dictionary2.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+import colorchange as color
+import sys
+import os
+from sklearn.preprocessing import PolynomialFeatures
+import joblib
+import time
+
+def dictionary(fileName,num,model):
+    m=cv2.imread(fileName)
+    h,w,bpp = np.shape(m)
+    num=num/10
+    colores={}
+    for py in range(0,h):
+        for px in range(0,w):
+                b=m[py][px][0]
+                g=m[py][px][1]
+                r=m[py][px][2] 
+                rgb = (r,g,b)
+                strDic=str(r)+","+str(g)+","+str(b)
+                if strDic in colores.keys():
+                    r,g,b=colores[strDic]
+                else:
+                    r,g,b=color.modify_rgb(rgb,model,1+num,0.5)
+                    colores[strDic]=(r,g,b)
+                m[py][px][0]=b
+                m[py][px][1]=g
+                m[py][px][2]=r
+                
+    cv2.waitKey(0)
+    newName=fileName.split(".")
+    newName="new-"+str(num)+"-"+newName[0]+"."+newName[1]
+    cv2.imwrite(newName,m)
+    return newName
+
+def combinar(fileName,newName,num):
+    m=cv2.imread(fileName)
+    m1=cv2.imread(newName)
+    numpy_horizontal = np.hstack((m, m1))
+    combinado=fileName.split(".")
+    num=num/10
+    combinado="c-"+str(num)+"-"+combinado[0]+"."+combinado[1]
+    cv2.imwrite(combinado,numpy_horizontal)
+    return combinado
+
+if __name__ == "__main__":
+    start_time = time.time()
+    fileName = str(sys.argv[1])
+    fileName=fileName.split('\\')[-1]
+    newDir=fileName.split(".")[0]
+    try:
+        os.makedirs("pruebas/"+newDir)
+    except:
+        cont=0
+        done=False
+        while(not done):
+            try:
+                newDir=newDir+"("+str(cont)+")"
+                os.makedirs("pruebas/"+newDir)
+                done=True
+            except :
+                cont+=1
+            
+    newNames=[]
+    model = joblib.load('model-x7.pkl')
+    i=0.3
+    newName=dictionary(fileName,i,model)
+    combinado=combinar(fileName,newName,i)
+    os.rename(newName,"pruebas/"+newDir+"/"+newName)
+    os.rename(combinado,"pruebas/"+newDir+"/"+combinado)
+    print("--- %s seconds ---" % (time.time() - start_time))
